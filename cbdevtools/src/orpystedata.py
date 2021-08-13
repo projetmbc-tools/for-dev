75acb4a53e069c8e7ed455ab394d476ec22bb48e
Christophe BAL <projetmbc@gmail.com>
2021-08-13 11:09:48 +0200

    cbdevtools: new sub minor verion 0.0.9-beta --> easy-to-use fixture datas builder

--- cbdevtools/src/orpystedata.py
+++ cbdevtools/src/orpystedata.py
@@ -10,8 +10,7 @@ from typing import *
 
 from pathlib import Path
 
-
-from orpyste.data import Read, ReadBlock
+from orpyste.data import ReadBlock
 
 
 ###
@@ -21,75 +20,88 @@ from orpyste.data import Read, ReadBlock
 #            this function from a testing file.
 #
 #     :return: = ; // See Python typing...
-#                an instance of ``ReadBlock`` obtained by analyzing
-#                a path::``PEUF`` file.
+#                an object containing the datas defined in a 
+#                path::``PEUF`` file (see the ¨info below).
 #
-#     :see: = _build_datas
+# This function returns an instance of ``ReadBlock`` obtained after 
+# analyzing a path::``peuf`` file automatically named. 
+# 
+# info::
+#     The name of the path::``peuf`` file is obtained by removing the prefix
+#     path::``test_`` of the name of the testing file (see ``file``).
 ###
 
 def build_datas_block(
     file: str,
 ) -> ReadBlock:
-    return _build_datas(
-        file = file,
-        clss = ReadBlock
-    )
-
-
-###
-# prototype::
-#     file = ; // See Python typing...
-#            just use the magic constant ``__file__`` when calling
-#            this function from a testing file.
-#
-#     :return: = ; // See Python typing...
-#                an instance of ``Read`` obtained by analyzing
-#                a path::``PEUF`` file.
-#
-#     :see: = _build_datas
-###
-
-def build_datas(
-    file: str,
-) -> Read:
-    return _build_datas(
-        file = file,
-        clss = Read
-    )
-
-
-###
-# prototype::
-#     file = ; // See Python typing...
-#            just use the magic constant ``__file__`` when calling
-#            this function from a testing file.
-#     clss = _ in [Read, ReadBlock] ; // See Python typing...
-#            the class to use to analyze the path::``PEUF`` file.
-#
-#     :return: = ; // See Python typing...
-#                an instance obtained by analyzing a path::``PEUF`` 
-#                file.
-#
-# This function returns an instance of either ``Read`` or ``ReadBlock`` 
-# obtained after analyzing a path::``PEUF`` file. 
-# 
-# info::
-#     The name of the path::``PEUF`` file is obtained by removing    
-#     path::``test_`` at the beginning of the name of the testing file 
-#     using either ``build_datas`` or ``build_datas_block``.
-###
-
-def _build_datas(
-    file: str,
-    clss: Union[Read, ReadBlock],
-) -> Union[Read, ReadBlock]:
     file    = Path(file)
     thisdir = file.parent
 
     whatistested = file.stem
     whatistested = whatistested.replace('test_', '')
 
-    return clss(
+    return ReadBlock(
         content = thisdir / f'{whatistested}.peuf',
         mode    = {"keyval:: =": ":default:"}
     )
+
+
+###
+# prototype::
+#     :see: = build_datas_block
+#
+# This fixture yields a ready-to-use data dictionary used to acheive the tests.
+# It also finalizes the cleaning of ¨orpyste extra files in case of problem. 
+#
+# info::
+#     The "intuitive" dictionary is build via ``mydict("std nosep nonb")``.
+#     See ¨orpyste.
+# 
+# 
+# Here is a real example of use.
+#
+# python::
+#     from cbdevtools import *
+#
+#     addsrc(
+#         file    = __file__,
+#         project = 'TeXitEasy',
+#     )
+#
+#     from src.escape import fstringit
+#
+#     def test_latex_use_fstringit(peuf_fixture):
+#         tests = peuf_fixture(__file__)
+#
+#         for infos in tests.values():
+#             found  = fstringit(infos['source'])
+#             wanted = infos['fstring']
+#
+#             assert wanted == found
+###
+
+# Refs
+#    * https://docs.pytest.org/en/6.2.x/fixture.html#scope-sharing-fixtures-across-classes-modules-packages-or-session
+#    * https://docs.pytest.org/en/6.2.x/fixture.html#factories-as-fixtures
+
+@fixture(scope = "session")
+def peuf_fixture():
+    datas_build = []
+
+###
+# prototype::
+#     file = ; // See Python typing...
+#            just use the magic constant ``__file__`` when calling
+#            this function from a testing file.
+###
+    def _make_peuf_datas(file):
+        datas_build.append(datas := build_datas_block(file))
+        
+        datas.build()
+
+        return datas.mydict("std nosep nonb")
+
+    yield _make_peuf_datas
+
+    for datas in datas_build:
+        datas.remove_extras()
