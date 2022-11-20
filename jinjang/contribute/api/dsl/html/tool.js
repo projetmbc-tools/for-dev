f361e5cce3fc83782a7a3f7cef8e113c04deba5d
Christophe BAL <projetmbc@gmail.com>
2022-11-20 01:03:18 +0100

    jinjaNG [yaml for config]

--- jinjang/contribute/api/dsl/html/tool.js
+++ jinjang/contribute/api/dsl/html/tool.js
@@ -1,6 +1,17 @@
-// Source: https://stackoverflow.com/a/74336061/4589608
+/**
+prototype::
+    action : ????
 
+
+ref::
+    The initial code comes from cf::``this post ; https://stackoverflow.com/a/74336061/4589608 ``.
+**/
 jinjang =_=>
   document.body.outerHTML=document.body.outerHTML
     .replace(/\[\[.*?\]\]/g, "")
-    .replace(/\{\{(.*?)\}\}/g, `<span style="color: red; font-weight: bold; border: solid 1px; padding: 1px 3px">$1</span>`);
+    .replace(
+        /\{\{(.*?)\}\}/g,
+        `<span style="color: red; font-weight: bold; `
+        +
+        `border: solid 1px; padding: 1px 3px">$1</span>`
+    );
