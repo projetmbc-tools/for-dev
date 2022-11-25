b3848c7c30317edb793115a6fdc0fdeca99586f7
Christophe BAL <projetmbc@gmail.com>
2022-11-25 23:51:33 +0100

    jinjaNG [flavour - continuing builder]

--- jinjang/tools/factory/dsl/build_01_update_specs.py
+++ jinjang/tools/factory/dsl/build_01_update_specs.py
@@ -36,9 +36,10 @@ TAB_3 = TAB_1*3
 
 
 DEFAULT_STATUS_CONTENT = {
-    "status" : 'pending',
+    "status" : 'on hold',
     "comment": (
-        'This specs are pending. '
+        'Specs on hold.'
+        ' '
         'The author of jinjaNG will contact you later.'
     )
 }
@@ -83,6 +84,114 @@ for specfile in CONTRIB_DSL_DIR.rglob("*/*specs.yaml"):
 # -- TOOLS FOR SOURCE -- #
 # ---------------------- #
 
+GRP_TAGS = {
+    (TAG_ABOUT := 'about'): [
+        TAG_AUTHOR:= 'author',
+        TAG_DESC  := 'desc',
+        TAG_DATE  := 'date',
+    ],
+    (TAG_SRC_COMMENT:= 'src-comment'): [
+        TAG_BLOCK := 'block',
+        TAG_INLINE:= 'inline',
+    ],
+    None: [
+        TAG_EXT  := 'ext',
+        TAG_VAR  := 'var',
+        TAG_PARAM:= 'param',
+    ]
+}
+
+
+TAG_1ST_NAME  = '1st name'
+TAG_LAST_NAME = 'last name'
+TAG_EMAIL     = 'email'
+
+
+def extractauthorinfos(text):
+    firstname, lastname = text.split(',')
+    lastname, email = lastname.split('[')
+
+    email = email.strip()[:-1]
+
+    infos = {
+        TAG_1ST_NAME : firstname,
+        TAG_LAST_NAME: lastname,
+        TAG_EMAIL    : email,
+    }
+
+    return {
+        k: v.strip()
+        for k,v in infos.items()
+    }
+
+
+def startend(text):
+    start, end = text.split('...')
+
+    return start.strip(), end.strip()
+
+
+def before(text):
+    bef, _ = text.split('...')
+
+    return bef.strip()
+
+
+FORMATERS = {
+    TAG_AUTHOR: extractauthorinfos,
+    TAG_BLOCK : startend,
+    TAG_INLINE: before,
+    TAG_VAR   : startend,
+}
+
+
+def specs2options(hardspec):
+    print(f"{TAB_2}+ Analyzing the hard specs...")
+
+    options = {}
+
+    for grptag, subtags in GRP_TAGS.items():
+        if grptag is None:
+            dict2use = hardspec.copy()
+
+        else:
+            dict2use = hardspec[grptag]
+
+        for tag, val in dict2use.items():
+            if not tag in subtags:
+                raise Exception(
+                    f"unknown key: {tag}.")
+
+            if tag in FORMATERS:
+                val = FORMATERS[tag](val)
+
+            options[tag] = val
+
+            if grptag is None:
+                del hardspec[tag]
+
+        if not grptag is None:
+            del hardspec[grptag]
+
+    if hardspec:
+        raise Exception(
+            f"unknown keys: {list(hardspec.keys())}."
+        )
+
+    print('OPTIONS >', options)
+
+    exit()
+
+
+
+
+
+
+
+# ---------------------- #
+# -- TOOLS FOR SOURCE -- #
+# ---------------------- #
+
 def update_src(hardspec):
     print(f"{TAB_2}+ Updating the source code...")
 
@@ -106,11 +215,12 @@ def update_doc(hardspec):
 # ------------------------ #
 
 for name, infos in allspecs.items():
-    if infos['status'] != 'ok':
-        print(f"{TAB_1}* REJECTED: {name}.")
-        continue
+    print(f"{TAB_1}* {name}.")
 
-    print(f"{TAB_1}* ACCEPTED: {name}.")
+    if infos['status'] != 'ok':
+        print(f"{TAB_2}+ {infos['status'].title()}.")
+
+        continue
 
     specdir = infos['dir']
 
@@ -120,5 +230,7 @@ for name, infos in allspecs.items():
     ) as f:
         hardspec = yaml_load(f)
 
-    update_src(hardspec)
-    update_doc(hardspec)
+    options = specs2options(hardspec)
+
+    update_src(options)
+    update_doc(options)
