bc064bbfd584b9ccbb601eb06ba4f5dc62a8fc39
Christophe BAL <projetmbc@gmail.com>
2022-12-17 22:54:28 +0100

    jinjaNG [Doc before code - CLI API - All + Mini tests in doc OK]

--- jinjang/doc/content/how-to/examples/hooks/pre/bash2datas.bash
+++ jinjang/doc/content/how-to/examples/hooks/pre/bash2datas.bash
@@ -3,6 +3,30 @@ NOW=$(date   "+%H:%M:%S")
 
 jq --null-input                    \
    --arg today "$TODAY"            \
-   --arg now   "$NOW"                \
+   --arg now   "$NOW"              \
    '{"day": $today, "time": $now}' \
 > lastchanges.json
+
+# ----------- #
+# -- TESTS -- #
+# ----------- #
+
+TEMPL_OUT="tmpl-test-out.json"
+
+if [ -f "$TEMPL_OUT" ]; then
+    rm "$TEMPL_OUT"
+fi
+
+touch  "$TEMPL_OUT"
+
+# {
+#     "day": {{day}},
+#     "time": {{time}}
+# }
+
+cat << EOT >> $TEMPL_OUT
+{
+    "day": "$TODAY",
+    "time": "$NOW"
+}
+EOT
