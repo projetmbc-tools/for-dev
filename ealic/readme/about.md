7945e08d256f1561c6dbf3aaca2dc0044c792409
Christophe BAL <projetmbc@gmail.com>
2023-01-03 16:49:26 +0100

    ealic - Hidden before the release of the new jinjaNG

--- ealic/readme/about.md
+++
@@ -1,4 +0,0 @@
-About `ealic`
--------------
-
-This project allows licenses to be added easily to electronic projects.
--- jinjaNG/build-project.bash
+++ jinjaNG/build-project.bash
@@ -5,5 +5,6 @@ THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECTNAME=$(basename "$THIS_DIR" | tr '[:upper:]' '[:lower:]')
 
 echo "Calling 'src2prod' to try to build a new '$PROJECTNAME' folder to publish it."
+echo ""
 
 python -m src2prod --usegit --notsafe --readme='readme' "$THIS_DIR"
