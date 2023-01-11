d1ba19428ff5a69f07fa2a88460df6baa1bbfc84
Christophe BAL <projetmbc@users.noreply.github.com>
2023-01-12 00:17:39 +0100

    jinjaNG [LaTeX - exavar.png - No more polution of the log?]

--- jinjaNG/contribute/api/dsl/latex/images/build_screenshots_slow.bash
+++ jinjaNG/contribute/api/dsl/latex/images/build_screenshots_slow.bash
@@ -18,12 +18,38 @@ cd images
 latexmk -pdf "exavar.tex"
 
 
+pdf2imag() {
+    convert -density 800 "$1" -alpha off -resize 14% "$2"
+}
+
+
 # ----------------- #
 # -- PDF --> PNG -- #
 # ----------------- #
 
-convert -density 800 "exavar.pdf" -alpha off -resize 14% "exavar.png"
+PDF_FILE="exavar.pdf"
 
+IMG_KEPT="exavar.png"
+IMG_CANDIDATE="exavar_candidate.png"
+IMG_DIFF="exavar_diff.png"
+
+if [ -f "exavar.png" ]
+then
+    pdf2imag $PDF_FILE $IMG_CANDIDATE
+
+# Sources.
+#     * https://imagemagick.org/script/compare.php
+#     * https://stackoverflow.com/a/38561570/4589608
+    metric=$(magick compare -metric PSNR $IMG_KEPT $IMG_CANDIDATE /dev/null 2>&1)
+
+    if [[ "$metric" != "0 (0)" ]]
+    then
+        cp $IMG_CANDIDATE $IMG_KEPT
+    fi
+
+else
+    pdf2imag $PDF_FILE $IMG_KEPT
+fi
 
 # ------------------------------ #
 # -- MINIMIZE OUR FINGERPRINT -- #
@@ -31,4 +57,4 @@ convert -density 800 "exavar.pdf" -alpha off -resize 14% "exavar.png"
 
 latexmk -C "exavar.tex"
 
-rm "jngutils.sty"
+rm $IMG_CANDIDATE "jngutils.sty"
